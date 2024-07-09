feea23a39742b43afdcfccfb78e3006c5b4422e0
rems <tech1.rems@gmail.com>
2024-07-09 22:18:33 +0000

    I changed the logic of the system to work in a way that, when users vote for candidates they are actually selecting and then at the end have a preview of their choices before submitting. So, the vote will be saved in the database when submitted at the preview. \n I also changed the root url to render the media files.

--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,25 @@
 from django.urls import path
 
-from .views import HomeView, BallotsView, VoteCompleteView
+from .views import HomeView, BallotsView, VoteCompleteView, PreviewVotesView
 from . import views
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("<slug:election_slug>/ballots/", BallotsView.as_view(), name="ballots"),
     path(
-        "vote/<int:election_id>/<int:candidate_id>/<int:position_id>/",
-        views.vote,
-        name="vote",
+        "vote/<slug:election_slug>/<int:position_id>/<int:candidate_id>/",
+        views.vote_candidate,
+        name="vote-candidate",
+    ),
+    path(
+        "<slug:election_slug>/preview/",
+        PreviewVotesView.as_view(),
+        name="preview-votes",
+    ),
+    path(
+        "submit_votes/<slug:election_slug>/",
+        views.submit_votes,
+        name="submit-votes",
     ),
     path("voting/complete/", VoteCompleteView.as_view(), name="vote-complete"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -46,42 +46,76 @@ class BallotsView(ListView):
     model = Position
     context_object_name = "candidates"
     template_name = "polls/ballots.html"
+    context_object_name = "positions"
     paginate_by = 1
     page_kwarg = "ballot"
 
-    # def get_queryset(self):
-    #     self.election = Election.objects.get(slug=self.kwargs["slug"])
-    #     queryset = Position.objects.
-    #     return queryset
+    def get_queryset(self):
+        queryset = Position.objects.all().order_by("id")
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         election = Election.objects.get(slug=self.kwargs["election_slug"])
         context["election"] = election
-        # context["position"] = self.position
         return context
 
 
-def vote(request, election_id, candidate_id, position_id):
-    if request.method == "POST":
-        # user = CustomUser.objects.get(id=candidate_id)
-        candidate = get_object_or_404(Candidate, id=candidate_id)
-        election = get_object_or_404(Election, id=election_id)
-        position = get_object_or_404(Position, id=position_id)
-        vote = Vote.objects.create(
-            voter=request.user,
-            candidate=candidate,
-            election=election,
-            position=position,
-        )
-        next_page = request.POST.get("ballot")
-        if not next_page:
-            return redirect("vote-complete")
+def vote_candidate(request, election_slug, candidate_id, position_id):
 
+    selected_candidates = request.session.get("selected_candidates", {})
+    selected_candidates[str(position_id)] = candidate_id
+    request.session["selected_candidates"] = selected_candidates
+    print(request.session["selected_candidates"])
+
+    next_page = request.GET.get("ballot")
+    if not next_page:
         return redirect(
-            reverse("ballots", kwargs={"election_slug": election.slug})
-            + f"?ballot={next_page}"
+            "preview-votes",
+            election_slug,
         )
+
+    return redirect(
+        reverse("ballots", kwargs={"election_slug": election_slug})
+        + f"?ballot={next_page}"
+    )
+    # return redirect("home")
+
+
+class PreviewVotesView(TemplateView):
+    template_name = "polls/preview_votes.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(PreviewVotesView, self).get_context_data(**kwargs)
+        election = Election.objects.get(slug=self.kwargs["election_slug"])
+        context["election"] = election
+
+        selected_candidates = self.request.session.get("selected_candidates")
+        context["selected_candidates"] = {
+            position_id: Candidate.objects.get(id=candidate_id)
+            for position_id, candidate_id in selected_candidates.items()
+        }
+
+        return context
+
+
+def submit_votes(request, election_slug):
+    if request.method == "POST":
+        election = Election.objects.get(slug=election_slug)
+
+        selected_candidates = request.session.get("selected_candidates")
+        for position_id, candidate_id in selected_candidates.items():
+            position = get_object_or_404(Position, id=position_id)
+            candidate = get_object_or_404(Candidate, id=candidate_id)
+            vote = Vote.objects.create(
+                voter=request.user,
+                candidate=candidate,
+                election=election,
+                position=position,
+            )
+        request.session["selected_candidates"] = {}
+        return redirect("vote-complete")
+
     return redirect("home")
 
 
