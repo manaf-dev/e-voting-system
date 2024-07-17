84b42d018941c593d31759bb97e7a7f36406f5ca
rems <tech1.rems@gmail.com>
2024-07-17 12:14:39 +0000

    Correct deployment config

--- voting_system/settings.py
+++ voting_system/settings.py
@@ -150,16 +150,6 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
